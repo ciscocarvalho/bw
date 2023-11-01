46cc465e6c80ec88eabcd2b0b0d39cf065cf345a
Francisco <francisco.carvalho.q@gmail.com>
2023-11-01 17:54:55 -0300

    fix tests
    see: https://github.com/chakra-ui/chakra-ui/issues/6036#issuecomment-1128368254

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,5 +8,8 @@ const config = {
   testEnvironment: "jest-environment-jsdom",
 };
 
+// https://github.com/chakra-ui/chakra-ui/issues/6036#issuecomment-1128368254
+export const setupFilesAfterEnv = ["<rootDir>/jest.setup.js"]
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
--- jest.setup.mjs
+++ jest.setup.mjs
@@ -1,3 +1,20 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+// https://github.com/chakra-ui/chakra-ui/issues/6036#issuecomment-1128368254
+beforeEach(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
